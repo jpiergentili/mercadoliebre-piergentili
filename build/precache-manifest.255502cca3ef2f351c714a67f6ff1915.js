9e8f64876c4459c425c320883272e04e508c5ba7
jpiergentili <javypier1@gmail.com>
2022-10-10 20:20:40 -0300

    Entrega final - v5a

---
+++ build/precache-manifest.255502cca3ef2f351c714a67f6ff1915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f6840864357bc607687",
+    "url": "/static/css/main.078d4050.chunk.css"
+  },
+  {
+    "revision": "5f6840864357bc607687",
+    "url": "/static/js/main.5f684086.chunk.js"
+  },
+  {
+    "revision": "15756aa9fef7983cc277",
+    "url": "/static/js/1.15756aa9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "47c06c32718f4a5cbd60a9cb23d5e895",
+    "url": "/index.html"
+  }
+];
